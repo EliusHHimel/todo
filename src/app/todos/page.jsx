30f90249d5fda28508715dfd4cc512af118d3630
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2024-01-03 13:09:04 +0600

    [add] button style

--- src/app/todos/page.jsx
+++ src/app/todos/page.jsx
@@ -6,15 +6,15 @@ const page = () => {
     return (
         <div className='flex justify-center flex-col items-center px-4 py-10 bg-slate-300'>
             <h1 className='text-2xl font-bold p-2'>Things I need to do</h1>
-            <div className="input_elements">
+            <div className="flex gap-3">
                 <input
                     className='p-2 rounded'
                     type="text"
                     placeholder='What to do?'
                     value={todos}
                     onChange={e => setTodos(e.target.value)} />
-                <button>Add</button>
-                <button>Clear</button>
+                <button className='text-white bg-green-700 px-5 rounded border border-green-700 hover:text-green-700 hover:bg-white'>Add</button>
+                <button className='text-white bg-red-700 px-5 rounded border border-red-700 hover:bg-white hover:text-red-700'>Clear</button>
             </div>
         </div>
     );
