d3a2508ee223c9c5cd9984032b182e6caf2525e5
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2024-01-03 16:02:07 +0600

    edit page added

--- src/app/todos/page.jsx
+++ src/app/todos/page.jsx
@@ -1,11 +1,27 @@
 "use client";
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
 const page = () => {
     const [inputText, setInputText] = useState("");
-    const [todos, setTodos] = useState([1, 2]);
+    const [todos, setTodos] = useState([1, 2, 3]);
+    const [editTodo, setEditTodo] = useState(false);
+
+    if (true) {
+        return (
+            <>
+                <div className='flex justify-center flex-col items-center px-4 py-10 bg-slate-300'>
+                    <h1 className='text-2xl font-bold mb-2'>Edit To Do</h1>
+                    <div className='flex gap-2'>
+                        <input className='p-2 rounded' type="text" placeholder='Edit To Do' />
+                        <input type="checkbox" name="" id="" />
+                        <button></button>
+                    </div>
+                </div>
+            </>
+        )
+    }
     return (
         <div className='flex justify-center flex-col items-center px-4 py-10 bg-slate-300'>
             <h1 className='text-2xl font-bold p-2'>Things I need to do</h1>
@@ -22,12 +38,12 @@ const page = () => {
             <div className="todo_list">
                 {todos.map((todo, index) => {
                     return (
-                        <div key={index} className='flex justify-between items-center p-2 bg-white rounded my-3 shadow-md gap-2'>
+                        <div key={index} className='flex justify-between items-center p-2 bg-white rounded my-3 shadow-md gap-2 w-60 pointer'>
                             <input type="checkbox" name="" id="" />
                             <div>{todo} Hi</div>
                             <div className='flex gap-2'>
-                                <FontAwesomeIcon icon={faPenToSquare} className='text-green-700' />
-                                <FontAwesomeIcon icon={faTrash} className='text-red-500' />
+                                <FontAwesomeIcon icon={faPenToSquare} className='text-green-700 cursor-pointer' />
+                                <FontAwesomeIcon icon={faTrashCan} className='text-red-500 cursor-pointer' />
                             </div>
                         </div>
                     )
